ad80e40727086d9ba5c590cd5abb2006b8979e51
tejanaveengit <peesatejanaveen@gmail.com>
2026-04-26 12:20:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+const express = require("express");
+const app = express();
 
+app.get("/", (req, res) => {
+  res.send("POC 24 - Node.js App running on ECS Fargate 🚀");
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "UP" });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
